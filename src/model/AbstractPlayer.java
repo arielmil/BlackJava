354ba06d1f59cdcc570b1b42746a30a3e3a575da
Ariel <mileguir23@hotmail.com>
2021-05-02 20:24:53 -0300

    Feitas alterações gerais

---
+++ src/model/AbstractPlayer.java
@@ -0,0 +1,49 @@
+/***********************************************************************/
+
+package model;
+
+/***********************************************************************/
+
+class AbstractPlayer {
+	private String name;
+	
+	/***************************************************************************
+	*  Method: Constructor - PlayerWrapper : Initializes the PlayerWrapper
+	*  ****/
+	
+	AbstractPlayer(String name) {
+		this.name = name;
+	}
+	/* End Method: Constructor - Player */
+	/*
+	***************************************************************************
+	*  Method: String getName : Returns the Name of the Player (!)
+	*  ****/
+	
+	String getName() {
+		return name;
+	}
+	/* End Method: Constructor - Player */
+	/*
+	****************************************************************************
+	 * 
+	 * To be implemented...
+	 * 
+	 * Method: Card Hit: The Player draws a card
+	 *  ****/
+	/*Card Hit() {
+		return Card;
+	}*/
+	/* End Method: Card Hit */
+	/*
+	***************************************************************************
+	*  Method: void Stand : The Player keeps his hand at it's current value
+	*  ****/
+	/*
+	void Stand() {
+		return;
+	}
+	*/
+	/* End Method: void Stand */
+	
+}
