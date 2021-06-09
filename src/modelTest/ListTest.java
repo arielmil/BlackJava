9dfebe823087c56136e34c835abd336c91b1b822
Pedro Gabriel <sales.pedrogabriel@gmail.com>
2021-06-09 07:50:06 -0300

    mais testes do List

--- src/modelTest/ListTest.java
+++ src/modelTest/ListTest.java
@@ -74,7 +74,7 @@ public class ListTest {
 	/*
 	****************************************************************************
 	* 
-	* Method: void testMoveFrom
+	* Method: void testCopyNTimes
 	*  ****/
 	
 	@Test 
@@ -93,7 +93,7 @@ public class ListTest {
         
     }
 	
-	/* End Method: testMoveFrom */
+	/* End Method: testCopyNTimes */
 	/*
 	****************************************************************************
 	* 
@@ -118,5 +118,143 @@ public class ListTest {
         
     }
 	
-	/* End Method: testMoveFrom */
+	/* End Method: testInsertL */
+	/*
+	****************************************************************************
+	* 
+	* Method: void testDrawL
+	*  ****/
+	
+	/*@Test 
+    public void testDrawL() {
+        List actual_list = new List();
+        List expected_list = new List();
+        int object_1 = 2;
+        int object_2 = 1;
+        int object_3 = 3;
+        
+        
+        actual_list.insertL(object_1);
+        expected_list.insertL(object_1);
+        actual_list.insertL(object_2);  
+        expected_list.insertL(object_2);
+        actual_list.insertL(object_3);
+        
+        assertEquals("Method testGetIndexGreaterThanSize Draw Equals was unsuccessfull", object_3,actual_list.drawL());
+        
+        actual_list.drawL();
+        
+        assertEquals("Method testGetIndexGreaterThanSize Equals Lists was unsuccessfull", expected_list,actual_list);
+    }*/
+	
+	/* End Method: testDrawL */
+	/*
+	****************************************************************************
+	* 
+	* Method: void GetSize
+	*  ****/
+	
+	@Test 
+    public void testGetSize() {
+        List actual_list = new List();
+        int expected_size = 1;
+        int object = 1;
+        		
+        actual_list.insertL(object);
+                           
+        assertEquals("Method testGetIndexLowerThanSize was unsuccessfull", expected_size,actual_list.getSize());
+        
+    }
+	
+	/* End Method: testGetSize */
+	/*
+	****************************************************************************
+	* 
+	* Method: void testUpdateSizeIncrease
+	*  ****/
+	
+	@Test 
+    public void testUpdateSizeIncrease() {
+        List actual_list = new List();
+        int object = 1;
+        		
+        actual_list.insertL(object);
+        actual_list.insertL(object);
+                                         
+        assertEquals("Method testUpdateSize Increase was unsuccessfull", 2,actual_list.size);
+        
+        
+    }
+	
+	/* End Method: testUpdateSizeIncrease */
+	/*
+	****************************************************************************
+	* 
+	* Method: void testUpdateSizeIncrease
+	*  ****/
+	
+	@Test 
+    public void testUpdateSizeDecrease() {
+        List actual_list = new List();
+        int object = 1;
+        		
+        actual_list.insertL(object);
+        actual_list.insertL(object);
+        actual_list.drawL();
+        
+        assertEquals("Method testUpdateSize Increase was unsuccessfull", 1,actual_list.size);
+        
+        
+    }
+	
+	/* End Method: testUpdateSizeDecrease */
+	/*
+	****************************************************************************
+	* 
+	* Method: void testClear
+	*  ****/
+	
+	@Test 
+    public void testClear() {
+        List actual_list = new List();
+        int object = 1;
+        
+        actual_list.insertL(object);
+        actual_list.clear();
+       
+        
+        assertEquals("Method testUpdateSize Increase was unsuccessfull", 0,actual_list.size);
+        
+        
+    }
+	
+	/* End Method: testClear */
+	/*
+	****************************************************************************
+	* 
+	* Method: void testDraw
+	*  ****/
+	/*
+	@Test 
+    public void testDrawAll() {
+        List actual_list = new List();
+        List drawed_list = new List();
+        List expected_list = new List();
+        int object_1 = 1;
+        int object_2 = 2;
+        
+        
+        actual_list.insertL(object_1);
+        actual_list.insertL(object_2);
+        drawed_list.insertL(object_1);
+        drawed_list.insertL(object_2);
+        expected_list = drawed_list.drawAll();
+       
+        
+        assertEquals("Method testdrawAll was unsuccessfull", expected_list,actual_list);
+        
+        
+    }*/
+	
+	/* End Method: testClear */
 }
