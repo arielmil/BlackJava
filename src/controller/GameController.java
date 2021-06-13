29377632f264df3d5447c74aac1dfbd03e8a9307
Pedro Gabriel <sales.pedrogabriel@gmail.com>
2021-06-13 13:26:17 -0300

    startando controller

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,29 +1,28 @@
 package controller;
 
-import model.Deck;
-import model.Player;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import view.BlackJavaFrame;
 
 public class GameController {
-    Deck deck;
-    List<Player> players;
-
-    public void startGame(int numberOfPlayers) {
-        players = new ArrayList<>(numberOfPlayers);
-        for (int i = 0; i < numberOfPlayers; i++) {
-            players.add(new Player(String.format("Player %d", i)));
-        }
+    
+	BlackJavaFrame mainFrame;
+    
+	public GameController () {
+		
+		mainFrame = new BlackJavaFrame();
+		
+		
+	}
+	
+    public void funcao1 () {
+    	
+    	
+    	
     }
-
-    public void makeBet(String playerName, int amount) {
-        for (Player player : players) {
-            if (Objects.equals(player.getName(), playerName)) {
-//                player.bet();
-                break;
-            }
-        }
+    
+    public static void main(String args[]) {
+    	
+    	GameController teste = new GameController();
+    	
     }
+    
 }
